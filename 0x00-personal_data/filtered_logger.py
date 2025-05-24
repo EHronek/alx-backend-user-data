64ca7eea57d686cddf91b597bd4749e060aa33b3
EHronek <emmanuelhronek@gmail.com>
2025-05-24 17:39:53 +0300

    regex-ing fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """Defines a function returns the log message obfuscated"""
 import re
+from typing import List
 
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """obfuscate specified fields in a log message"""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
