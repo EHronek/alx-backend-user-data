c785bc99fb16c56416389b0814ea00d0f3f29993
EHronek <emmanuelhronek@gmail.com>
2025-05-24 13:45:49 +0300

    regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Defines a function returns the log message obfuscated"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """obfuscate specified fields in a log message"""
+    return re.sub(
+            rf'({"|".join(fields)})=[^{separator}]*',
+            rf'\1={redaction}',
+            message)
