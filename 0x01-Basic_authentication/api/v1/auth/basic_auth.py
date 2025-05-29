f50f58a0879d7829ff62ab6c75234e133747ec20
EHronek <emmanuelhronek@gmail.com>
2025-05-29 18:17:33 +0300

    basic auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basic authentication module for the API"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class for basic authentication"""
+    pass
