2365f98729f382a4b2b66378d6a3fc0bf207e76f
EHronek <emmanuelhronek@gmail.com>
2025-05-29 19:23:13 +0300

    user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ import re
 import binascii
 import base64
 from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -51,3 +52,20 @@ class BasicAuth(Auth):
                 password = field_match.group('password')
                 return user, password
         return None, None
+
+    def user_object_from_credentials(
+            self, 
+            user_email: str, user_pwd: str
+            ) -> TypeVar('User'):
+        """Retrieves a user based on the users
+        authentication credentials"""
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None
