1599b6d9e7561deb18fb8d1b7feb58979eded46b
EHronek <emmanuelhronek@gmail.com>
2025-05-30 19:57:18 +0300

    in memory session lint fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,12 +8,11 @@ class SessionAuth(Auth):
     """Session authentication class"""
     user_id_by_session_id = {}
 
-    def create_session(self, user_id:str = None) -> str:
+    def create_session(self, user_id: str = None) -> str:
         """creates a session id for user_id"""
         if user_id is None:
             return None
         if type(user_id) is str:
             session_id = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
-            return session_id
-        
+            return session_id  
