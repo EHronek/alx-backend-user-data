13e2757dec0224085d212838c81455e0f2d571f2
EHronek <emmanuelhronek@gmail.com>
2025-05-30 19:55:44 +0300

    in memory session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 """Defines a class that authenticates a Session"""
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session authentication class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id:str = None) -> str:
+        """creates a session id for user_id"""
+        if user_id is None:
+            return None
+        if type(user_id) is str:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        
