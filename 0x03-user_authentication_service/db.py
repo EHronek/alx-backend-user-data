d56dd341dfb946965c63c150b49d85824edc0988
EHronek <emmanuelhronek@gmail.com>
2025-06-04 09:06:32 +0300

    create a new user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -24,7 +24,7 @@ class DB:
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
-        return self.__session    
+        return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database"""
