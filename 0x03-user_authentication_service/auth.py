dc23930e995735a42fee111d29c67073f8f7e7c6
EHronek <emmanuelhronek@gmail.com>
2025-06-05 11:03:06 +0300

    update password endpoint and logic

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,3 +91,19 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """updates a specific users password based in the reset_token"""
+        user = None
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except No ResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        new_password = _hash_password(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=new_password,
+            reset_token=None
+        )
