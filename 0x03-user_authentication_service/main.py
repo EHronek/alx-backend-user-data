00ec6781359a7d2894eba89564a258836eca2112
EHronek <emmanuelhronek@gmail.com>
2025-06-05 08:11:44 +0300

    is valid login

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -89,3 +89,19 @@ try:
     print("successfully created a new user!")
 except ValueError as err:
     print("could not create a new user: {}".format(err))
+
+print()
+print("IS VALID USER")
+
+
+email = 'bob@bob.com'
+password = 'MyPwdOfBob'
+auth = Auth()
+
+auth.register_user(email, password)
+
+print(auth.valid_login(email, password))
+
+print(auth.valid_login(email, "WrongPwd"))
+
+print(auth.valid_login("unknown@email", password))
